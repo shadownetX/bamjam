042d3dba84edbba0d961e103659ba1da7de508b9
shadownetX <thomas@thiebaudet.fr>
2019-10-08 04:36:15 +0200

    fix(bazar) load fixtures and format

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -76,8 +76,8 @@ class User implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        // guarantee every user at least has ROLE_WRITER
+        $roles[] = 'ROLE_WRITER';
 
         return array_unique($roles);
     }
